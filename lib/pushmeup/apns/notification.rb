88ef30ab3a51563bc4f75e863e1d418bdbac6a17
Jakob <jakob.boss@gmail.com>
2014-05-28 16:50:19 +0200

    Added support for custom APS attributes

--- lib/pushmeup/apns/notification.rb
+++ lib/pushmeup/apns/notification.rb
@@ -31,7 +31,10 @@ module APNS
       aps['aps']['alert'] = self.alert if self.alert
       aps['aps']['badge'] = self.badge if self.badge
       aps['aps']['sound'] = self.sound if self.sound
+
+      aps['aps'].merge!(self.other.delete 'aps') if self.other.is_a?(Hash)
       aps.merge!(self.other) if self.other
+
       aps.to_json.gsub(/\\u([\da-fA-F]{4})/) {|m| [$1].pack("H*").unpack("n*").pack("U*")}
     end
 
